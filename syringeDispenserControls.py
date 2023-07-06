a03d3bf6f2cd2f458ad7befa721521f829660b85
SaidGLopez12 <Armoniel+Github@proton.me>
2023-07-06 11:01:59 -0700

    update of syringeControls
    testing the analog input and output channels

--- syringeDispenserControls.py
+++ syringeDispenserControls.py
@@ -1,22 +1,30 @@
-import nidaqmx # library for national instruments controls
+import nidaqmx # library for national instruments controls| For DAC USB 6009
 import time # for delay functions
-task = nidaqmx.Task() 
 
+#--Analog Output Channels--#
+task = nidaqmx.Task() 
 task.ao_channels.add_ao_voltage_chan('Dev1/ao0', 'Channel_0', min_val = 0.00, max_val = 5.00) # create a analog output channel for voltage at channel ao0 called channel_1
 task.start() # Required | Turn on the channel
-value = 2
-task.write(value) # Set the Channel's voltage to 2
-time.sleep(2)
-task.write(value+3) # Set the Channel's voltage to 5
 
-task_2 = nidaqmx.Task()
-task_2.ao_channels.add_ao_voltage_chan('Dev1/ao1', 'Channel_1', min_val = 0.00, max_val = 5.00)
-task_2.start() # required for channel 2
-task_2.write(3)
+
+#--Analog Input Channels--#
+task_read = nidaqmx.Task() # creating a function to read voltage from the Syringe Dispenser
+task_read.ai_channels.add_ai_voltage_chan('Dev1/ao1') # create an analog input channel
+task_read.start() # start the analog input channel
+
+#------------------------#
+
+value = input()
+task.write(0) # Set the Channel's voltage to 2
+time.sleep(2)
+task.write(value) # Set the Channel's voltage to 5
+
+value = task_read.read()
+print(f"The current voltage for the voltage channel is: {value}")
 
 task.stop() # Required | Stop the channel
 task.close() # Required | Close the channel
 
-task_2.stop()
-task_2.close()
+task_read.stop()
+task_read.close()
 
