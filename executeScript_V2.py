b1a14b16f64eb2441a1b6ad543bc3114cb43b2a0
SaidGLopez12 <Armoniel+Github@proton.me>
2023-07-31 15:35:04 -0700

    update current Execute Script
    - tested some async properties to run both functions at the same time. It works but it runs into issues after both events are done.

--- executeScript_V2.py
+++ executeScript_V2.py
@@ -4,12 +4,13 @@ import os
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+from threading import Thread
 
 from AconitySTUDIO_client import AconitySTUDIOPythonClient 
 from AconitySTUDIO_client import utils
 
 #from powerSupplyControls import timerFunction, heatPadOneChannel, heatPadMultipleChannels
-#from syringeDispenserControls import dispenseOperation
+from syringeDispenserControls import dispenseOperation
 #-------------------------------------#
 
 # -- Initial Variables and Scripts -- # 
@@ -24,25 +25,28 @@ platformRearPos = "30"
 
 # AconityScripts for SLIDER
 #returnPos = f'$m.move_abs($c[slider], {positiveEndPos}, 250)'
-initalPos = '$m.move_abs($c[slider], 80,250)' # move slider to first pos of deposition process
+initalPos = '$m.move_abs($c[slider], 60,250)' # move slider to first pos of deposition process
 finalPos = '$m.move_abs($c[slider], 165,10)' # move slider to final pos of deposition process | Get's called with the syringe operation function
+# initalPos = '$m.move_abs($c[slider], 165,200)' # move slider to first pos of deposition process
+# finalPos = '$m.move_abs($c[slider], 75,10)' # move slider to final pos of deposition process | Get's called with the syringe operation function
 centerPos = f'$m.move_abs($c[slider],{centerSlidePos},250)' # this will be used to move slider away from the laser during the sintering process | Default Pos too
  
 # ------------------------------------ #
 
 # -- Platform Movement Variables And AconityScripts-- #
-defaultHeight = "15.95"
+defaultHeight = "18.050"
 layersProcessed = 0
-platformIncrementUp = -2
-platfromDecrementDown = 2 
+layerThickness = 0.05
+platformIncrementUp = -2.00
+platfromDecrementDown = 2.00
 
 # AconityScript for PLATFORM Movement
 defaultPlatformHeight = f'$m.move_abs($c[platform],{defaultHeight}, 200)'
 slotDiePlatFormHeight_UP = f'$m.move_rel($c[platform],-1.95,200)' # decreasing the value makes the platform go up
 slotDiePlatFormHeight_DOWN = f'$m.move_rel($c[platform],2,200)' #increasing the value makes the platform go down
 
-slotDiePlatFormHeight_UP2 = f'$m.move_rel($c[platform],{str(platformIncrementUp)},200)'
-slotDiePlatFormHeight_DOWN2 = f'$m.move_rel($c[platform],{str(platfromDecrementDown)},200)'
+slotDiePlatFormHeight_UP2 = f'$m.move_rel($c[platform],{platformIncrementUp},200)'
+slotDiePlatFormHeight_DOWN2 = f'$m.move_rel($c[platform],{platfromDecrementDown},200)'
 
 #-------------------------------------#
 
@@ -93,13 +97,25 @@ sinterTechniques = {
 
 execution_script = sinterTechniques['single_Scan']
 build_parts = 'all'
-start_layer = 1
+start_layer = 8
 currentLayer = start_layer
 initalLayer = start_layer # This is only ever used in the multi layer, but only once
-end_layer = 4
+end_layer = 10
 layersProcessed = 0 
 
+#--------------------------------#
+# -- Threading -- #
+async def dispensingFun(client):
+    await client.execute(channel = 'manual_move', script = finalPos)
+
+t1 = Thread(target=dispensingFun)
+t2 = Thread(target=dispenseOperation)
 # ---------------------------------------------------------------#
+
+
+
+
+
 
 async def executeFunc(login_data, info):
     
@@ -116,8 +132,11 @@ async def executeFunc(login_data, info):
      
 
      os.system('cls' if os.name == 'nt' else 'clear') # Clear everything above this code | To get rid of clutter
-     #await singleLayerPILMFunc(client)
-     await multiLayerPILMFun(client, currentLayer, layersProcessed, platformIncrementUp, platfromDecrementDown)
+    #  await client.execute(channel = 'manual_move', script = initalPos)
+    #  await asyncio.sleep(1)
+    #  await client.execute(channel = 'manual_move', script = finalPos)
+     await singleLayerPILMFunc(client)
+    #  await multiLayerPILMFun(client, currentLayer, layersProcessed, platformIncrementUp, platfromDecrementDown)
      
 
 async def multiLayerPILMFun(client, currentLayer, layersProcessed, platformIncrementUp, platfromDecrementDown):
@@ -130,23 +149,19 @@ async def multiLayerPILMFun(client, currentLayer, layersProcessed, platformIncre
      # Loop for Layering Process
      while currentLayer <= end_layer:
          print(f"Current Layer: {currentLayer}\n")
-         
-        #  if layersProcessed == 0:
-        #      await client.execute(channel = 'manual_move', script = defaultPlatformHeight) # incase the platfrom is not in this position
-         if layersProcessed > 0:
-             platformIncrementUp -= -0.05
-             await client.execute(channel = 'manual_move', script = slotDiePlatFormHeight_UP2)
+        
+         if layersProcessed > 1:
+             await client.execute(channel = 'manual_move', script = slotDiePlatFormHeight_DOWN2)   
 
         # Deposition Process Preparation
          await asyncio.sleep(5)
          await client.execute(channel = 'manual_move', script = centerPos)
          await asyncio.sleep(1)
-         await client.execute(channel = 'manual_move', script = initalPos)
             
         # Slot Die Deposition Process
-         await asyncio.sleep(1)
-         await client.execute(channel = 'manual_move', script = finalPos)
-        #  dispenseOperation()  # incase the slider is not in this position from the start
+         await client.execute(channel = 'manual_move', script = initalPos)
+         await asyncio.sleep(2)
+         await despositionProcess(client)
          await asyncio.sleep(10) # wait for the slider to dispense the ink on the substrate
         #PSU function Goes Here
         
@@ -164,16 +179,26 @@ async def multiLayerPILMFun(client, currentLayer, layersProcessed, platformIncre
          await client.pause_job() # pause the job    
          
         # Preparing for next layer
-         if layersProcessed == 0 :
-            await client.execute(channel = 'manual_move', script = slotDiePlatFormHeight_DOWN2)
-         if layersProcessed > 0:
-             platfromDecrementDown += 0.05
-             await client.execute(channel = 'manual_move', script = slotDiePlatFormHeight_DOWN2)
+         if layersProcessed == 1 :
+            await client.execute(channel = 'manual_move', script = slotDiePlatFormHeight_UP2)
+       
+         if layersProcessed > 1 :
              
+             platformIncrementUp += layerThickness # e.g -2.00 + 0.05 = -1.95 -> new platformIncrementUp value
+             await client.execute(channel = 'manual_move', script = slotDiePlatFormHeight_UP2)
+
+            
          layersProcessed += 1  
              
      await client.stop_job()
+
+async def despositionProcess(client):
+    dispenseOperation()
+    await client.execute(channel = 'manual_move', script = finalPos)
+    # loop = coroutine 
     
+
+     
 async def singleLayerPILMFunc(client):
      await client.execute(channel = 'manual_move', script = defaultPlatformHeight)
     
@@ -188,17 +213,23 @@ async def singleLayerPILMFunc(client):
      
      # Slot Die Deposition Process
      await asyncio.sleep(1)
-     await client.execute(channel = 'manual_move', script = finalPos)
-     #dispenseOperation()  # incase the slider is not in this position from the start
-     await asyncio.sleep(5) # wait for the slider to dispense the ink on the substrate
+     
+     await despositionProcess(client)
+    #  await client.execute(channel = 'manual_move', script = finalPos)
+    #  loop.run_in_executor(func=dispenseOperation())
+       # incase the slider is not in this position from the start
+     await asyncio.sleep(15) # wait for the slider to dispense the ink on the substrate
      #PSU function Goes Here
      
      # Start Sintering Process
      await client.execute(channel='manual_move', script=centerPos) # Move it back to starting position
      await asyncio.sleep(5)
      await client.start_job(execution_script = execution_script,layers = [start_layer, end_layer],parts = build_parts) 
-     await asyncio.sleep(5) # Varies. * Something to adjust *   
-         
+    #  await asyncio.sleep(200) # Varies. * Something to adjust *   
+
+
+
+
 # Conditional statment will cause the program to be executed if it's condition is met.
 if __name__ == '__main__': # Required * Explain *
     
